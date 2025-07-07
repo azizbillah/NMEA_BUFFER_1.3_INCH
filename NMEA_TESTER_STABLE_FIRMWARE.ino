7b8bd6863bd832123a9fc8ce76458a4196db39d9
azizbillah <64129354+azizbillah@users.noreply.github.com>
2025-07-07 22:02:45 +0700

    Update NMEA_TESTER_STABLE_FIRMWARE.ino

--- NMEA_TESTER_STABLE_FIRMWARE.ino
+++ NMEA_TESTER_STABLE_FIRMWARE.ino
@@ -1,54 +1,22 @@
 
-// #include <SPI.h>
-// #include <Wire.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
-// #include <Adafruit_SH110X.h>
-// #include <Fonts/TomThumb.h>
-// #include <Fonts/FreeSerif9pt7b.h>
-// #include <Fonts/FreeMono9pt7b.h>
-#include <PubSubClient.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-#include <Fonts/TomThumb.h>
-#include <Fonts/FreeSerif9pt7b.h>
-#include <Fonts/FreeMono9pt7b.h>
-#include <HardwareSerial.h>
-#include <ezButton.h>
-//
-//
-//#define I2C_SDA 22
-//#define I2C_SCL 21
-//
-//// #define I2C_SDA 22
-//// #define I2C_SCL 21
-//#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
-//
-//
-//#define SCREEN_WIDTH 128 // OLED display width, in pixels
-//#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-//
-//// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-//#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-////Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-//Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#include <Adafruit_SSD1306.h>
 
-// Declaration for an SH1106 display connected to I2C (SCL=D1, SDA=D2 pins)
-#define SCREEN_ADDRESS 0x3C //< See datasheet for Address; 0x3C for 128x64
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-Adafruit_SH1106G oled = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1, 400000, 100000);
 
-
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 unsigned long prevByteMillis = 0;
 unsigned long curMillis;
 
 
-const int recPin = 5;
+const int recPin = 3;
 long baudRate;
 long rate=10000;
 
@@ -60,257 +28,194 @@ boolean parsing=false;
 int x, minX;
 
 void setup() 
-{   
-    
-    Wire.begin(I2C_SDA, I2C_SCL);
-    display.begin(i2c_Address, true);
-    display.clearDisplay();
-    display.drawRect(1, 1, display.width() - 2, display.height() - 2, SH110X_WHITE);
-    display.setTextColor(SH110X_WHITE);
-    //display.setFont(&FreeMono9pt7b);
-    display.setTextSize(2);
-    display.setCursor(44,10);             // Start at top-left corner
-    display.print("NMEA");
-    display.setCursor(36,28); 
-    display.print("BUFFER");
-    display.setTextSize(1);
-    display.setCursor(30,46);
-    display.print("M2N.Instument");
-    display.display();
-
-
-    pinMode(recPin, INPUT);
+{   pinMode(recPin, INPUT);
     digitalWrite(recPin, HIGH);
     baudRate = detRate(recPin);  
-    Serial2.begin(baudRate);
-    Serial.begin(115200);
+    Serial.begin(baudRate);
     delay(100);
-    Serial2.flush();  
+    Serial.flush();  
     dataIn="";  
+    
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+    }    
 }
-
  
 void loop() 
 {     
-     curMillis = millis();    
-     display.clearDisplay();
-     display.setTextColor(SH110X_WHITE);        // Draw SH110X_WHITE text   
-     display.drawRect(1, 1, display.width() - 2, display.height() - 2, SH110X_WHITE);
-     if(Serial2.available()>0)
+     
+      curMillis = millis();    
+      display.clearDisplay();
+      display.setTextSize(2);            
+      display.setCursor(12,12);
+      display.setTextColor(WHITE);        // Draw white text    
+     if(Serial.available()>0)
      {
-           char inChar = (char)Serial2.read();
+           char inChar = (char)Serial.read();
            dataIn += inChar;
            if (inChar == '\n') {parsing = true;}
      }
      if(parsing)
      {     
+          
            parsingData();
            parsing=false;
            dataIn="";
            prevByteMillis = curMillis; 
-     }
 
-     if (curMillis - prevByteMillis >=5000) {
-               display.clearDisplay();
-               display.drawRect(1, 1, display.width() - 2, display.height() - 2, SH110X_WHITE);
-               display.setTextSize(2);             // Normal 1:1 pixel scale
-               display.setTextColor(SH110X_WHITE);        // Draw SH110X_WHITE text
-               display.setCursor(22,6);             // Start at top-left corner
-               display.print("NO DATA");
-               display.setTextSize(1.5);
-               display.setCursor(24,30);             // Start at top-left corner
-               display.print("Or Press Reset");
-               display.setCursor(10,46);             // Start at top-left corner
-               display.print("to Change Baudrate");
-               display.display();
-               display.clearDisplay();
            
-      }
+         
+
+     }
+ 
+      if (curMillis - prevByteMillis >=5000) {
+                display.clearDisplay();
+                display.setTextSize(2);             // Normal 1:1 pixel scale
+                display.setTextColor(WHITE);        // Draw white text
+                display.setCursor(12,10);             // Start at top-left corner
+                display.print("NO DATA");
+
+                display.setTextSize(1);  
+                display.setCursor(2,28);             // Start at top-left corner
+                display.print("Or Change Baudrate");
+                display.display();
+                display.clearDisplay();
             
+       }
+      
+     
+
+       
+
+   
+    
+      
 }
  
 void parsingData()
 {
       int j=0;
       dt[j]="";
+      //proses parsing data
       for(i=1;i<dataIn.length();i++)
       {
+             //pengecekan tiap karakter dengan karakter (#) dan (,)
              if ((dataIn[i] == '$') || (dataIn[i] == ','))
              {
-                  j++;
-                  dt[j]="";
+                   //increment variabel j, digunakan untuk merubah index array penampung
+                   j++;
+                  dt[j]=""; //inisialisasi variabel array dt[j]
              }
              else
              {
+                  //proses tampung data saat pengecekan karakter selesai.
                   dt[j] = dt[j] + dataIn[i];
              }
       }
+          if(dt[0] == "GPRMC"){
+          float lat = dt[3].toFloat();
+          static char buff[10];
+          dtostrf( lat,7,3,buff);
+          Serial.print(buff);
+          display.print(buff);
+          display.display();
+          display.clearDisplay();
+      }
+      
 
-      if(dt[0] == "GPRMC"){ 
-            float lat,lat1,long_, long_1;
-            static char buff[10], buff1[10];
-            String lat_d ="";
-            String long_d="";
-            lat = dt[3].toFloat();
-            if(dt[4]=="S"){
-              lat1= -(lat/100);
-            }
-            else if(dt[4]=="N"){
-              lat1= (lat/100);
-              
-            }
-            dtostrf(lat1, 4,4,buff);
-            lat_d += buff;
+//      if(dt[0] == "SDDPT"){
+//          display.setTextSize(2);   
+//           
+//          display.setCursor(0,12);
+//          display.print("STNC:"); 
+//
+//          display.setCursor(60,12);
+//          display.print(dt[0]);
+//
+//          display.setCursor(0,28);  
+//          display.print("DPTH:");
+//
+//           
+//          display.setCursor(60,28);  
+//          display.print(dt[1]);
+//
+//          
+//          display.display();
+//          delay(100);
+//          display.clearDisplay();
+//      }
+//
+//      else if(dt[0] == "HCHDT"){
+//          display.setTextSize(2);   
+//           
+//          display.setCursor(0,12);
+//          display.print("STNC:"); 
+//
+//          display.setCursor(60,12);
+//          display.print(dt[0]);
+//
+//          display.setCursor(0,28);  
+//          display.print("HDG :");
+//
+//           
+//          display.setCursor(60,28);  
+//          display.print(dt[1]);
+//
+//          
+//          display.display();
+//          delay(100);
+//          display.clearDisplay();
+//      }
+//
+//      else if(dt[0] == "GPRMC"){
+//
+//        
+//          float lat ;
+//          float long_;
+//          char buffer1[10];
+//          char buffer2[10];
+//          lat = dt[3].toFloat();
+//          long_ = dt[5].toFloat();
+//
+//          dtostrf( lat , 7, 3, buffer1);
+//          
+//          dtostrf( long_ , 7,3, buffer2);
+//       
+//          display.setTextSize(1);   
+//           
+//          display.setCursor(0,8);
+//          display.print("STNC:"); 
+//
+//          display.setCursor(40,8);
+//          display.print(dt[0]);
+//
+//          display.setCursor(0,26);  
+//          display.print("LAT :");
+//
+//           
+//          display.setCursor(40,26);  
+//          display.print(buffer1);
+//
+//          display.setCursor(0,44);  
+//          display.print("LONG:");
+//
+//          display.setCursor(40,44);  
+//
+//          
+//          display.print(buffer2);
+//
+//          display.display();
+//          delay(100);
+//          display.clearDisplay();
+//      }
+//
 
-            long_ = dt[5].toFloat();
-            long_1= long_/100;
-            dtostrf(long_1, 4,4,buff1);
-            long_d += buff1;
-            display.setTextSize(2);            
-            display.setCursor(34,4);
-            display.print(dt[0]);            
-            display.setCursor(24,24);
-            display.print(lat_d);       
-            display.setCursor(20,44);
-            display.print(long_d);
-            display.display();
-            display.clearDisplay();
-            
-       } 
-
-       else if ( dt[0].charAt(2)=='H' && dt[0].charAt(3)=='D' && dt[0].charAt(4)== 'T'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-            
-        
-       }
-       else if ( dt[0].charAt(2)=='H' && dt[0].charAt(3)=='D' && dt[0].charAt(4)== 'M'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-            
-        
-       }
-       else if ( dt[0].charAt(2)=='M' && dt[0].charAt(3)=='T' && dt[0].charAt(4)== 'W'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='D' && dt[0].charAt(3)=='P' && dt[0].charAt(4)== 'T'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='D' && dt[0].charAt(3)=='B' && dt[0].charAt(4)== 'T'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[3]);
-            display.display();
-            display.clearDisplay();
-       }
-       
-       else if ( dt[0].charAt(2)=='V' && dt[0].charAt(3)=='H' && dt[0].charAt(4)== 'W'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[5]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='M' && dt[0].charAt(3)=='W' && dt[0].charAt(4)== 'D'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='M' && dt[0].charAt(3)=='W' && dt[0].charAt(4)== 'V'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='V' && dt[0].charAt(3)=='W' && dt[0].charAt(4)== 'R'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-       else if ( dt[0].charAt(2)=='H' && dt[0].charAt(3)=='D' && dt[0].charAt(4)== 'G'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-
-       else if ( dt[0].charAt(2)=='V' && dt[0].charAt(3)=='B' && dt[0].charAt(4)== 'W'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setCursor(20,38);
-            display.print(dt[1]);
-            display.display();
-            display.clearDisplay();
-       }
-
-       else if ( dt[0].charAt(2)=='V' && dt[0].charAt(3)=='D' && dt[0].charAt(4)== 'M'){
-
-            display.setTextSize(3);            
-            display.setCursor(20,10);
-            display.print(dt[0]);
-            display.setTextSize(2);  
-            display.setCursor(52,38);
-            display.print("AIS");
-            display.display();
-            display.clearDisplay();
-       }
+      
 
 
 
-
-                
+           
 }
 
 long detRate(int recpin)  // function to return valid received baud rate
@@ -329,7 +234,7 @@ long detRate(int recpin)  // function to return valid received baud rate
     */
    long baud,  x;
    
-   for (int m = 0; m < 5; m++)
+   for (int i = 0; i < 5; i++)
      {
       while(digitalRead(recpin) == 1){} // wait for low bit to start
       x = pulseIn(recpin,LOW);   // measure the next zero bit width
@@ -360,5 +265,3 @@ long detRate(int recpin)  // function to return valid received baud rate
       baud = 0;  
    return baud; 
   }
-
-  
